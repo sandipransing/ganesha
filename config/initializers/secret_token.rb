108ebfd6d4659273ded87298a13ffe57f162989d
Sandip Ransing <sandip@joshsoftware.com>
2011-11-14 20:36:34 +0530

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cdc::Application.config.secret_token = 'cab7247b709d96bfe0642d1519d7071421b96e04a6ceb009716f5dec4dc28cfe324033220a1c9514664e02e314143b2a05b0a85698cb8ce2a2fe938de1dcba87'
